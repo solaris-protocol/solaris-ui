0ebad1ee564d9b08ee345f3f362465b9b319f3c3
Alexey Elizarov <felex-ae@ya.ru>
2021-05-25 02:23:54 +0300

    refactor: wdyr to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
-import "./wdyr";
-
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
+if (process.env.NODE_ENV === "development") {
+    const whyDidYouRender = require("@welldone-software/why-did-you-render");
+    whyDidYouRender(React, {
+        trackAllPureComponents: true,
+    });
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
