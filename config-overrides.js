7fe4aabde45baa490e633aa922c9a64c85302505
Alexey Elizarov <felex-ae@ya.ru>
2021-05-25 04:34:56 +0300

    refactor: react-app-rewired, linaria, remove and update dependencies

---
+++ config-overrides.js
@@ -0,0 +1,32 @@
+const {
+  useBabelRc,
+  override,
+  addWebpackModuleRule,
+  addLessLoader,
+} = require("customize-cra");
+
+const isDev = process.env.NODE_ENV === "development";
+
+module.exports = override(
+  useBabelRc(),
+  addLessLoader({
+    lessOptions: {
+      modifyVars: { "@primary-color": "#2abdd2" },
+      javascriptEnabled: true,
+    },
+  }),
+  addWebpackModuleRule({
+    test: /\.tsx?$/,
+    exclude: /node_modules/,
+    use: [
+      { loader: "babel-loader" },
+      {
+        loader: "@linaria/webpack-loader",
+        options: {
+          cacheDirectory: "src/.linaria_cache",
+          sourceMap: isDev,
+        },
+      },
+    ],
+  })
+);
