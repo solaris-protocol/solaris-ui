2db6806d22e7d8764f4b441535d4ae2825059b8b
Alexey Elizarov <felex-ae@ya.ru>
2021-06-07 18:30:18 +0300

    refactor: clear

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,4 +1,2 @@
-export * from "./labels";
-export * from "./math";
-export * from "./marks";
-export * from "./style";
+export * from './labels';
+export * from './math';
--- src/routes.tsx
+++ src/routes.tsx
@@ -5,23 +5,6 @@ import { Borrow } from 'pages/borrow';
 import { Deposit } from 'pages/deposit';
 
 import { Layout } from './components/common/Layout';
-// import {
-//   BorrowReserveView,
-//   BorrowView,
-//   DashboardView,
-//   DepositReserveView,
-//   DepositView,
-//   FaucetView,
-//   HomeView,
-//   LiquidateReserveView,
-//   LiquidateView,
-//   MarginTrading,
-//   RepayReserveView,
-//   ReserveView,
-//   TransactionListView,
-//   WithdrawView,
-// } from './old/views';
-// import { NewPosition } from './old/views/margin/newPosition';
 import { Providers } from './Providers';
 
 export const Routes: FC = () => {
@@ -37,50 +20,6 @@ export const Routes: FC = () => {
               <Route exact path="/borrow">
                 <Borrow />
               </Route>
-              {/*<Route path="/borrow/:id">*/}
-              {/*  <BorrowReserveView />*/}
-              {/*</Route>*/}
-
-              {/*<Route exact path="/home">*/}
-              {/*  <HomeView />*/}
-              {/*</Route>*/}
-              {/*<Route exact path="/dashboard">*/}
-              {/*  <DashboardView />*/}
-              {/*</Route>*/}
-              {/*<Route exact path="/transactions">*/}
-              {/*  <TransactionListView />*/}
-              {/*</Route>*/}
-              {/*<Route path="/reserve/:id">*/}
-              {/*  <ReserveView />*/}
-              {/*</Route>*/}
-
-              {/*<Route path="/deposit/:id">*/}
-              {/*  <DepositReserveView />*/}
-              {/*</Route>*/}
-              {/*<Route path="/withdraw/:id">*/}
-              {/*  <WithdrawView />*/}
-              {/*</Route>*/}
-              {/*<Route path="/repay/loan/:obligation">*/}
-              {/*  <RepayReserveView />*/}
-              {/*</Route>*/}
-              {/*<Route path="/repay/:reserve">*/}
-              {/*  <RepayReserveView />*/}
-              {/*</Route>*/}
-              {/*<Route exact path="/liquidate">*/}
-              {/*  <LiquidateView />*/}
-              {/*</Route>*/}
-              {/*<Route path="/liquidate/:id">*/}
-              {/*  <LiquidateReserveView />*/}
-              {/*</Route>*/}
-              {/*<Route exact path="/margin">*/}
-              {/*  <MarginTrading />*/}
-              {/*</Route>*/}
-              {/*<Route path="/margin/:id">*/}
-              {/*  <NewPosition />*/}
-              {/*</Route>*/}
-              {/*<Route exact path="/faucet">*/}
-              {/*  <FaucetView />*/}
-              {/*</Route>*/}
             </Switch>
           </Layout>
         </Providers>
